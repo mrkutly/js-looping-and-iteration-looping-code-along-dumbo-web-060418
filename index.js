1e238c25cadb9ccee48e396871d0244a7a0b70b3
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-15 12:32:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function printBadges(names){
+  for(let i = 0; i < names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+  return names;
+}
+
+function trueOrFalse(){
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let tailsCount = 0;
+
+  while (trueOrFalse()){
+    ++tailsCount;
+  }
+  return `You got ${tailsCount} tails in a row!`;
+}
